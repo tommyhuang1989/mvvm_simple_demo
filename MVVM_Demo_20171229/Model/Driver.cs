1d94de43ba927e4bf1d192e50f31d80d59f31660
Tommy Huang <tommy.huang@Easiware.org>
2018-01-05 18:11:46 +0800

    add the DataTemplate example to GitLab

--- MVVM_Demo_20171229/Model/Driver.cs
+++ MVVM_Demo_20171229/Model/Driver.cs
@@ -11,6 +11,17 @@ namespace MVVM_Demo_20171229.Model
         public string Mfg { get; set; }
         public string Date { get; set; }
         public string Version { get; set; }
-        
+        public Driver()
+        {
+
+        }
+
+        public Driver(String name, String mfg, String date, String version)
+        {
+            Name = name;
+            Mfg = mfg;
+            Date = date;
+            Version = version;
+        }
     }
 }
--- MVVM_Demo_20171229/ViewModels/MainWindowViewModel.cs
+++ MVVM_Demo_20171229/ViewModels/MainWindowViewModel.cs
@@ -4,13 +4,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Windows;
+using System.Collections.ObjectModel;
 
 namespace MVVM_Demo_20171229.ViewModels
 {
     public class MainWindowViewModel:NotificationObject
     {
         public DelegateCommand ClickCommand { get; set; }
-
+        
         private Driver driver;
 
         public Driver Driver
@@ -22,6 +23,20 @@ namespace MVVM_Demo_20171229.ViewModels
                 this.RaisePropertyChanged("Driver");
             }
         }
+
+        private ObservableCollection<Driver> driverList;
+
+        public ObservableCollection<Driver> DriverList
+        {
+            get { return driverList; }
+            set
+            {
+                driverList = value;
+                this.RaisePropertyChanged("DriverList");
+            }
+        }
+        
+        
         
 
         public MainWindowViewModel()
@@ -34,6 +49,20 @@ namespace MVVM_Demo_20171229.ViewModels
             Driver.Mfg = "戴尔";
             Driver.Date = "2017-12-29";
             Driver.Version = "v1.0";
+
+            InitData();
+        }
+
+        private void InitData()
+        {
+            DriverList = new ObservableCollection<Driver>() { 
+                new Driver("HP driver", "HP", "2018-01-06", "22.33.45"),
+                new Driver("ASUS video driver", "ASUS", "2018-01-06", "22.33.45"),
+                new Driver("HP driver", "HP", "Lenovo", "22.33.45"),
+                new Driver("HP driver", "HP", "NVIDA", "22.33.45"),
+                new Driver("HP driver", "HP", "AMD", "22.33.45"),
+                new Driver("HP driver", "HP", "Gigabyte", "22.33.45"),
+            };
         }
 
         private void Click(object obj)
